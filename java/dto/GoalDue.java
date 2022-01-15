8a525ab259de43a3e435ee8945a516a90750b9ee
robinjoon <0108dlatnqls@gmail.com>
2022-01-16 00:06:40 +0900

    add String to GoalDue convert method

--- java/dto/GoalDue.java
+++ java/dto/GoalDue.java
@@ -1,9 +1,12 @@
 package dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum GoalDue {
-	ONE_YEAR,
-	SIX_MONTHS,
-	THREE_MONTHS,
-	ONE_WEEK,
-	NEXT_YEAR;	
+	ONE_YEAR, SIX_MONTHS, THREE_MONTHS, ONE_WEEK, NEXT_YEAR;
+
+	@JsonCreator
+	public static GoalDue from(String s) {
+		return GoalDue.valueOf(s.toUpperCase());
+	}
 }
