57b18cf63be5a5498fc5e812fd008d1cbac72af7
robinjoon <0108dlatnqls@gmail.com>
2022-01-24 00:24:26 +0900

    add a goal inquiry function by email

--- java/controller/GoalController.java
+++ java/controller/GoalController.java
@@ -1,12 +1,17 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import dao.GoalDao;
 import dto.*;
@@ -24,4 +29,14 @@ public class GoalController {
 			return ResponseEntity.badRequest().body(null);
 		}
 	}
+	@GetMapping("/{email:.+}")
+	public ResponseEntity<List<Goal>> getGoalList(@PathVariable("email") String email){
+		List<Goal> result = dao.selectGoals(email);
+		if(result.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}else {
+			return ResponseEntity.ok(result);
+		}
+	}
+
 }
