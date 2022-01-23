f6ea018d8370e81b7b88c785807bb8872c6c621d
robinjoon <0108dlatnqls@gmail.com>
2022-01-24 00:26:08 +0900

    modify Spring Config

--- java/config/MvcConfig.java
+++ java/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -18,6 +19,15 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		// TODO Auto-generated method stub
+		configurer.favorPathExtension(false);
+		/*
+		 * email 을 url로 받을 때 .을 확장자로 인식하지 않도록 하기 위한 설정.
+		 */
 	}
 
 }
